c29ec1f25f1911306e844d162ac56fc26228c7e1
Barış İnandıoğlu <binandioglu2006@gmail.com>
2021-08-10 08:40:20 +0300

    mouseup event updated to mousedown event

--- static/js/dismissHandler.js
+++ static/js/dismissHandler.js
@@ -1,4 +1,4 @@
-document.addEventListener('mouseup', function(e) {
+document.addEventListener('mousedown', function(e) {
     var container = document.getElementById('login-popup');
     if (!container.contains(e.target)) {
         container.parentElement.style.display = 'none';
