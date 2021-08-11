6259125e5058caf98387414cf17df8c586daa382
Barış İnandıoğlu <binandioglu2006@gmail.com>
2021-08-11 19:31:27 +0300

    progress

--- app.py
+++ app.py
@@ -1,30 +1,41 @@
 from flask import *
 from os import path
 
-app = Flask(__name__,
-            template_folder='src')
+app = Flask(__name__, template_folder='src')
+
 
 @app.route('/')
 def home():
     return render_template('./screens/index.html')
 
+
 @app.route('/about')
 def about():
     return render_template('./screens/about.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('./screens/register.html')
+
 
 @app.route('/contribute')
 def contribute():
     return render_template('./screens/contr.html')
 
+
 @app.route('/favicon.png')
 def favicon():
     return send_from_directory(path.join(app.root_path, 'static'),
-    'favicon.png',mimetype='image/vnd.microsoft.icon')
+                               'favicon.png',
+                               mimetype='image/vnd.microsoft.icon')
+
 
 # not found
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('./err/404.html'), 404
+
 
 if __name__ == '__main__':
     app.run(debug=True)
