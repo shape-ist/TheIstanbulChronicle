c1a6c3d76c72f1727f2250c0f8cb12b93147a0ec
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-08-17 22:36:49 +0300

    remove ts

--- static/ts/dismissHandler.ts
+++
@@ -1,8 +0,0 @@
-var ignoreClickOnMeElement = document.getElementById('someElementID');
-
-document.addEventListener('click', function(event) {
-    var isClickInsideElement = ignoreClickOnMeElement.contains(event.target);
-    if (!isClickInsideElement) {
-        //Do something click is outside specified element
-    }
-});z
