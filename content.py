041f7084e5e4bc866bf59d0d155c28b17d6148eb
Barış İnandıoğlu <binandioglu2006@gmail.com>
2021-08-23 09:53:25 +0300

    yaml to yml

--- content.py
+++ content.py
@@ -1,6 +1,6 @@
-def load_content():
+def load_content(yaml_file):
     import yaml
-    with open("content.yaml", 'r') as stream:
+    with open(yaml_file, 'r') as stream:
         try:
             return yaml.safe_load(stream)
         except yaml.YAMLError as e:
