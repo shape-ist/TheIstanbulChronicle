b62227f1de5875f1113dfe5558646c4a16b3cd48
Barış İnandıoğlu <binandioglu2006@gmail.com>
2021-08-26 16:48:33 +0300

    write button

--- static/js/checkScroll.js
+++ static/js/checkScroll.js
@@ -2,11 +2,13 @@ function isScrolledToTop() {
 
     if (window.scrollY > 100) {
         document.getElementById("header").style.boxShadow = "var(--bs)";
+        document.getElementById("header").style.background = "var(--bg-alt)";
     }
     else {
         document.getElementById("header").style.boxShadow = "unset";
+        document.getElementById("header").style.background = "var(--bg)";
     }
-    setTimeout(isScrolledToTop, 500);
+    setTimeout(isScrolledToTop, 50);
 }
 
 isScrolledToTop();
