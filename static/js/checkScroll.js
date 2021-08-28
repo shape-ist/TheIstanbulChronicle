81fb354e6c19c87971b354001ee06c983aadfedc
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-08-28 21:50:04 +0300

    mde css

--- static/js/checkScroll.js
+++ static/js/checkScroll.js
@@ -1,10 +1,8 @@
 function headerState() {
     if (window.scrollY > 100) {
         document.getElementById("header").style.boxShadow = "var(--bs)";
-        document.getElementById("header").style.background = "var(--bg-alt)";
     } else {
         document.getElementById("header").style.boxShadow = "unset";
-        document.getElementById("header").style.background = "var(--bg)";
     }
 }
 
