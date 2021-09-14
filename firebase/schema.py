fa7fde2c3e94cfd49d6fb9c96d6f46e08cf8ce89
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-09-14 19:21:39 +0300

    reformatting

--- firebase/schema.py
+++ firebase/schema.py
@@ -34,21 +34,21 @@ def article(db=db,
             tag: str = '',
             article_type: str = '',
             cover_image: str = ''):
-            article_type: str = '',
-            is_approved: bool = False):
-    from time import time
-    return {
-        u'title': title,
-        u'body': body,
-        u'writer_uid': db.collection(u'users').document(writer_uid),
-        u'article_type': article_type,
-        u'is_approved': False,
-        u'timestamp': time(),
-        u'cover_image': cover_image
-        u'is_approved': is_approved
-        # TODO: implement tags here ^^^^
-    }
+    article_type: str = '',
+    is_approved: bool = False):
 
+
+return {
+    u'title': title,
+    u'body': body,
+    u'writer_uid': db.collection(u'users').document(writer_uid),
+    u'article_type': article_type,
+    u'is_approved': False,
+    u'timestamp': time(),
+    u'cover_image': cover_image
+    u'is_approved': is_approved
+    # TODO: implement tags here ^^^^
+}
 
 """
     The schema module will be used like so:
