36e0f688dc9245dfb932ea4eebcf7942f92376df
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-09-20 01:06:19 +0300

    add edit profile page

---
+++ static/js/filterInput.js
@@ -0,0 +1,22 @@
+function phoneRegexFilter(textbox, inputFilter) {
+    ["input", "keydown", "keyup", "mousedown", "mouseup", "select", "contextmenu", "drop"].forEach(function (event) {
+        textbox.addEventListener(event, function () {
+            if (inputFilter(this.value)) {
+                this.oldValue = this.value;
+                this.oldSelectionStart = this.selectionStart;
+                this.oldSelectionEnd = this.selectionEnd;
+            } else if (this.hasOwnProperty("oldValue")) {
+                this.value = this.oldValue;
+                this.setSelectionRange(this.oldSelectionStart, this.oldSelectionEnd);
+            } else {
+                this.value = "";
+            }
+        });
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function (event) {
+    phoneRegexFilter(document.getElementById("profile-edit-phone"), function (value) {
+        return /^[\d +()-]*$/.test(value);
+    });
+})
