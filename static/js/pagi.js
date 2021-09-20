0f18549730cfe7851b61fe6e785a8bc9c77a791e
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-09-20 17:32:03 +0300

    init anime.js

--- static/js/pagi.js
+++ static/js/pagi.js
@@ -5,6 +5,10 @@ function inArea(threshold = 400){
 function triggerPagi() {
     if (inArea()){
         console.log("call paginator function here")
+        // get number of children in article-list
+        // this will wait until the paginator runs so (theoretically) there won't be a repeated paginator function.
+        // get new number of children in article-list
+        // compare numbers of children in article-list, only continue if they are not equivalent.
     }
 }
 
