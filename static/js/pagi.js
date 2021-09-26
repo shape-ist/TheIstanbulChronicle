04d2a04d7b864bf834c3b4c7454ddfe2157e2c25
Barış İnandıoğlu <68742481+baris-inandi@users.noreply.github.com>
2021-09-26 15:48:24 +0300

    finally have a working pagination frontend. wish me luck on petite vue

--- static/js/pagi.js
+++ static/js/pagi.js
@@ -1,17 +1,36 @@
-function inArea(threshold = 400){
-    return $(window).scrollTop() + $(window).height() > $(document).height() - threshold
+function inArea(threshold = 100, current = $(window).scrollTop()){
+    return current + $(window).height() > $(document).height() - threshold
 }
 
-function triggerPagi() {
-    if (inArea()){
-        console.log("call paginator function here")
-        // get number of children in article-list
-        // this will wait until the paginator runs so (theoretically) there won't be a repeated paginator function.
-        // get new number of children in article-list
-        // compare numbers of children in article-list, only continue if they are not equivalent.
-    }
+function getPagi(props){
+    return Promise.resolve($.ajax({
+        url: `/api/pagi/articles/timestamp/q?${props}`
+    }));
+  }
+
+var pagiState = false;
+function triggerPagi(last_uid=null) {
+    pagiState = true;
+    if (last_uid == null){index = ''}
+    else {index = `&i=${last_uid}`}
+    getPagi(`o=desc&l=2${index}`)
+    .then(data => o = data)
+    .catch(error => console.log('Pagination Error:', error));
+    $('#article-list').height("+=1000");
+    pagiState = false;
+    return o
+    // get number of children in article-list
+    // this will wait until the paginator runs so (theoretically) there won't be a repeated paginator function.
+    // get new number of children in article-list
+    // compare numbers of children in article-list, only continue if they are not equivalent.
 }
+
+/* var intervalId = window.setInterval(function(){
+    if (inArea()) {triggerPagi()}
+}, 500); */
 
 var intervalId = window.setInterval(function(){
-    triggerPagi()
+    if ($('#pagi-trigger').visible() && !pagiState){
+        console.log(triggerPagi())
+    }
 }, 500);
