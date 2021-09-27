b06f967f7ca3aacef72f658ae94ef21580b522f9
Barış İnandıoğlu <binandioglu2006@gmail.com>
2021-09-27 15:21:26 +0300

    debug functions

--- firebase/debug.py
+++ firebase/debug.py
@@ -1,9 +1,20 @@
 from firebase import user
-
-
-def init_user():
-    return user.register(f"debug@debug.com", "debug1234", "Ada Lovelace")
-
+from firebase import article
+from firebase import schema
 
 def debug_user():
-    return user.login(f"debug@debug.com", "debug1234")
+    try:
+        try:
+            return user.login(f"debug@debug.com", "debug1234")
+        except:
+            return user.register(f"debug@debug.com", "debug1234", "ada lovelace")
+    except: raise Exception('debug user auth. failed.')
+
+
+def test_articles():
+    for i in range(50):
+        article.upload_article(schema.article(
+            title = f'{i+1}- Article title',
+            body = 'Lorem ipsum dolor sit amet constrectur adispiscing elit.',
+            cover_image = 'https://udiscoverbrand.co/img/products/84396-goel-tekne-agaclar-daglar-bulutlar-doga-manzara-255fj-odas-ev-duvar-modern-sanat-dekor-ahsap-cerceve-poster.jpg'
+        ))
