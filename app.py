8bdf3aeb4a888c5df83b3486bbbd20616f3c4799
baris-inandi <binandioglu2006@gmail.com>
2021-10-09 16:37:02 +0300

    init breakpoints

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ def home():
             raise Exception('Authentication Failed')
     init_pagi = paginate.paginate('articles', 'timestamp', l=5, o='DESC')
     for i in init_pagi['data'][1:]:
-        i['body'] = i['body'].strip().replace("\n", "")[:240].rsplit(' ', 1)[0]
+        i['body'] = i['body'].strip().replace("\n", "")[:200].rsplit(' ', 1)[0]
     print(init_pagi['data'])
     return render_template('./screens/index.html',
                            subpage=subpage,
