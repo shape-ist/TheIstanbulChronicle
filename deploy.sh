3ca1950ce73e25cde608f9d89faae3b19082be88
baris-inandi <binandioglu2006@gmail.com>
2021-10-09 19:17:12 +0300

    Release: v0.0.11-test

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,7 @@
 g='\033[1;32m' # green
 r='\033[1;31m' # red
 p='\033[1;35m' # purple
+c='\033[1;36m' # cyan
 w='\033[0m' # white
 printf "\n## ${p} Welcome to the Chroncile deployer ${w} ##\n\n"
 
@@ -28,7 +29,7 @@ fi
 releasetag="v${VERSION}${VERSIONSTATUS}"
 
 while true; do
-    printf "${g}Release: ${w}Proceed releasing ${releasetag}? (Y/n) "
+    printf "${g}Release: ${w}Proceed releasing ${c}${releasetag}${w}? (Y/n) "
     read yn
     case $yn in
         [Yy]* ) break;;
@@ -37,13 +38,17 @@ while true; do
     esac
 done
 
+printf "${c}INFO: ${w}Committing release to remote... "
 git pull -q
 git commit -q -a -m "Release: ${releasetag}"
 git push -q
 
 gh release create $releasetag
+printf "${c}INFO: ${w}Deploying code..."
 heroku git:remote -a istchron
 mv .gitignore .gitignore-deploy
 git push heroku main
+printf "${c}INFO: ${w}Deploy complete, cleaning up..."
 mv .gitignore-deploy .gitignore
 git remote rm heroku
+printf "${p}\nSuccesfully deployed the Chronicle!\n${w}"
